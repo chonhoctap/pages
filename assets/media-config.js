e2a3c5f9cf29ada9d5c86a393f319bd305b9c276
Chốn Học Tập <chonhoctap.vn@gmail.com>
2026-07-30 17:03:12 +0700

    Thêm cấu hình endpoint media R2

---
+++ assets/media-config.js
@@ -0,0 +1,4 @@
+// Sau khi deploy Cloudflare Worker, thay chuỗi rỗng bằng URL workers.dev.
+// Không thêm dấu / ở cuối. Ví dụ:
+// export const MEDIA_API_URL = 'https://chonhoctap-media.example.workers.dev';
+export const MEDIA_API_URL = '';
